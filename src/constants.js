8cdb64cf28206a542e1cc47eb12885b8744a8553
miaocunhui <miaocunhui@baoxiaohe.com>
2022-06-16 16:14:23 +0800

    feat: 新增repoList

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 const { version } = require('../package.json');
 const repoList = [
-    { projectName: 'vite_vue3_ts', url: 'https://github.com/yasuomang/template-vue3-ts.git' },
-    { projectName: 'vue2_js', url: '' }
+    { projectName: 'vite_vue3_ts_pc', url: 'https://github.com/yasuomang/template-vue3-ts.git' },
+    { projectName: 'vite_vue3_ts_mobile', url: 'https://github.com/yasuomang/template-vue3-ts-m.git' },
+    { projectName: 'lib', url: 'https://github.com/yasuomang/template-lib.git' },
+    { projectName: 'vue2_js', url: '' },
 ]
 module.exports = {
   version,
