d9e6d1786cce53b9db93c418f39242c1eac35d6d
miaocunhui <miaocunhui@baoxiaohe.com>
2022-06-16 14:07:18 +0800

    feat: 更换项目地址

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const { version } = require('../package.json');
 const repoList = [
-    { projectName: 'vite_vue3_ts', url:'https://github.com/yasuomang/vite_vue3_ts.git' },
-    { projectName: 'vue2_js', url:'' }
+    { projectName: 'vite_vue3_ts', url: 'https://github.com/yasuomang/template-vue3-ts.git' },
+    { projectName: 'vue2_js', url: '' }
 ]
 module.exports = {
   version,
