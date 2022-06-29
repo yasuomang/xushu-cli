c88114e1bdde4b39b2900904f642885fdebc1b85
miaocunhui <miaocunhui@baoxiaohe.com>
2022-06-29 18:19:42 +0800

    feat: add koa2 template

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ const repoList = [
     { projectName: 'vite_vue3_ts_pc', url: 'https://github.com/yasuomang/template-vue3-ts.git' },
     { projectName: 'vite_vue3_ts_mobile', url: 'https://github.com/yasuomang/template-vue3-ts-m.git' },
     { projectName: 'lib', url: 'https://github.com/yasuomang/template-lib.git' },
+    { projectName: 'koa2_ts_sequelize', url: 'https://github.com/yasuomang/template-koa-ts.git' },
     { projectName: 'vue2_js', url: '' },
 ]
 module.exports = {
